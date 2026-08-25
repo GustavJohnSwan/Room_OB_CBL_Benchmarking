7ffac4fe6080c4791a095d7addab35b43d579318
Agressive_Swan <gustavs.janis.gulbis@gmail.com>
2026-08-25 15:48:36 +0300

    Done : - Project Build restored to functional state - Added default values for all OB DB model entity values - Researched recommended class/function heirachy structure - Started implementing proper class/function structure with JSON reader - Put desired JSON files in dedicated assets folder - JsonReader logic - JsonParser logic
    Next :
    
    - develop buisness logic to input JSON->listOfDataObjects into OB database
    - write a delete all entries query for database cleanup
    - develop basic CRUD for OB
    - test basic CRUD on inserted JSON data in OB database
    - move on to do all the same with Room ...

--- app/src/main/java/com/bignerdranch/android/room_ob_cbl_benchmarking/buisness_logic/json_data_tests.kt
+++ app/src/main/java/com/bignerdranch/android/room_ob_cbl_benchmarking/buisness_logic/json_data_tests.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.room_ob_cbl_benchmarking.buisness_logic
 
 import android.util.Log
 
+// this was made to test if the read actually happenned. You can delete this function and its call
 fun json_data_tests(jsonString: String?) {
     Log.d("JsonReader", "Char count in Json string : ${jsonString?.count()}")
 }

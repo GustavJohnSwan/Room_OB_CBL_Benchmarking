5244d5278d15c485aafebbd7f3406bfbd310fd66
Agressive_Swan <gustavs.janis.gulbis@gmail.com>
2026-08-26 22:12:45 +0300

    Done : - Project Build restored to functional state - Added default values for all OB DB model entity values - Researched recommended class/function heirachy structure - Started implementing proper class/function structure with JSON reader - Put desired JSON files in dedicated assets folder - JsonReader logic - JsonParser logic
    Next :
    
    - develop buisness logic to input JSON->listOfDataObjects into OB database
    - write a delete all entries query for database cleanup
    - develop basic CRUD for OB
    - test basic CRUD on inserted JSON data in OB database
    - move on to do all the same with Room
    - move on to do all the same with CBL ...

--- app/src/main/java/com/bignerdranch/android/room_ob_cbl_benchmarking/buisness_logic/data_mapping/OB_Mapping.kt
+++ app/src/main/java/com/bignerdranch/android/room_ob_cbl_benchmarking/buisness_logic/data_mapping/OB_Mapping.kt
@@ -1,13 +1,30 @@
 package com.bignerdranch.android.room_ob_cbl_benchmarking.buisness_logic.data_mapping
 
 import com.bignerdranch.android.room_ob_cbl_benchmarking.buisness_logic.json_Operations.GeneratedEvent
+import com.bignerdranch.android.room_ob_cbl_benchmarking.database.EntryOb_B
+import com.bignerdranch.android.room_ob_cbl_benchmarking.database.ExtraDataOb_B
+import io.objectbox.BoxStore
 
-class OB_Mapping () {
+class OB_Mapping (store: BoxStore) {
+
+    val EOBBox = store.boxFor(EntryOb_B::class.java)
+    val EDOBBox = store.boxFor(ExtraDataOb_B::class.java)
 
     fun map(listOfDataObjects: List<GeneratedEvent>) {
 
+
+
         listOfDataObjects.forEach {
 
+            // TEST IF THIS WORKES
+
+            val entryEntity = EntryOb_B(
+                dateOb = it.date,
+                entryOb = it.title,
+                timeMinutesOb = it.time
+            )
+
+            EOBBox.put(entryEntity)
         }
 
     }
